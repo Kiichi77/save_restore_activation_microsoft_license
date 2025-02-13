82c5ee73a6013844f19bff5f91a1f1078cb8f956
🌊喜市🌊 <Kiichi@Kiichi-MobileWS.kiichi.local>
2025-02-13 16:36:52 +0800

    Update the bat files to handles backups in the folders named by hostname

--- restore_activation.bat
+++ restore_activation.bat
@@ -6,11 +6,32 @@ rem REPOSITORY
 rem https://github.com/thelogh/save_restore_activation_microsoft_license
 rem V.1.0.0
 
+echo Do not connect your PC or SERVER to the internet until the license has been restored!!!
+echo Please make sure the internet is disconnected before you continue.
+pause
+
+rem Check the backup directory exist or not
+if not exist "backups\%HOST%" echo The directory that holds backed up licenses for this PC is not exist (Maybe wrong hostname). && goto end
+if not exist "backups\%HOST%\license" echo The 'license' directory is missing. && goto end
+if not exist "backups\%HOST%\GenuineTicket" echo The 'GenuineTicket' directory is missing. && goto end
+if not exist "backups\%HOST%\winactivationkey.reg" echo File 'winactivationkey.reg' is missing && goto end
+
+rem The files are ready, go on to restore license(s)
+
+rem Stop the services
 net stop clipsvc
 net stop sppsvc
-xcopy .\license\2.0\ C:\Windows\System32\spp\store\2.0 /O /X /E /H /K /Y
-xcopy .\GenuineTicket\tokens.dat C:\ProgramData\Microsoft\Windows\ClipSVC /O /X /H /K /Y
-copy /Y .\winactivationkey.reg C:\Windows\System32\winactivationkey.reg
+
+xcopy .\backups\%HOST%\license\2.0\ C:\Windows\System32\spp\store\2.0 /O /X /E /H /K /Y
+xcopy .\backups\%HOST%\GenuineTicket\tokens.dat C:\ProgramData\Microsoft\Windows\ClipSVC /O /X /H /K /Y
+copy /Y .\backups\%HOST%\winactivationkey.reg C:\Windows\System32\winactivationkey.reg
 psexec -i -s -accepteula reg import C:\Windows\System32\winactivationkey.reg
 del C:\Windows\System32\winactivationkey.reg
+
+echo Restoring license is finished. You may need to check whether there is some errors in the output above.
+echo You computer will reboot after you press any key.
+pause >nul
 shutdown /r /t 0
+
+:end 
+echo Please check make sure the backup directory is existed and retry by running this programme again.
--- save_activation.bat
+++ save_activation.bat
@@ -1,3 +1,4 @@
+@echo off
 rem Offline script to save and restore activation for Microsoft Windows and Office licenses by Thelogh
 rem Backup and Restore Microsoft Windows and Microsoft Office License Activation Offline
 rem https://www.alldiscoveries.com/backup-and-restore-microsoft-windows-and-microsoft-office-license-activation-offline/
@@ -6,16 +7,27 @@ rem REPOSITORY
 rem https://github.com/thelogh/save_restore_activation_microsoft_license
 rem V.1.0.0
 
+rem Get hostname
+for /f "tokens=* USEBACKQ" %%F in (`hostname`) do (set HOST=%%F)
+
+rem Create directories for placing the license and token file.
+if not exist "backups\%HOST%\license" mkdir "backups\%HOST%\license"
+if not exist "backups\%HOST%\GenuineTicket" mkdir "backups\%HOST%\GenuineTicket"
+
+rem Stop the services to get the license and token file
 net stop clipsvc
 net stop sppsvc
 xcopy C:\Windows\System32\spp\store .\license /O /X /E /H /K /Y
-
-net stop clipsvc
-net stop sppsvc
 xcopy C:\ProgramData\Microsoft\Windows\ClipSVC\tokens.dat .\GenuineTicket /O /X /H /K /Y
 
+rem Dump the licnese in registry 
 psexec -i -s -accepteula reg export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\{7746D80F-97E0-4E26-9543-26B41FC22F79} .\winactivationkey.reg
-
-copy C:\Windows\System32\winactivationkey.reg .\
-
+copy C:\Windows\System32\winactivationkey.reg .\backups\%HOST%\
 del C:\Windows\System32\winactivationkey.reg
+
+rem Bring back the services
+net start clipsvc
+net start sppsvc
+
+echo Your licnese file is located at .\backups\%HOST%\, please keep it safe.
+pause
